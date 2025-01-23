17d92e1cdfa0a570eabb4a4f572a4365fed1503e
majie <373002712@qq.com>
2025-01-23 16:58:32 +0800

    feat: button:request 内容支持格式化

--- src/components/button/interface.ts
+++ src/components/button/interface.ts
@@ -70,6 +70,7 @@ export type BasePropTypes = {
     zIndex?: number;
     icon?: ReactNode;
   };
+  confirmBeforeClickFormatter?: (tableProps: any) => Object;
   onBeforeClick?: (arg0: BeforeClickOption) => Promise<boolean>;
   onEmit?: (eventName: string, onEmit?: any) => void;
   buttonProps?: ButtonProps;
