e2f5b2196a926a9e8b2f2a1087b3b2414fbeadf2
ljb <13657086451@163.com>
2025-03-06 18:09:25 +0800

    feat: buttonlist 新增uiType upload

--- src/components/button/upload.tsx
+++ src/components/button/upload.tsx
@@ -1,29 +1,97 @@
-import React from 'react';
-import { Button, message } from 'antd';
-import Upload, { PropTypes } from '@/components/upload';
-import { UploadButtonProps } from './interface';
+import React, { useEffect, useState, useRef } from 'react';
+import { Button, Upload } from 'antd';
+import type { RcFile, UploadProps } from 'antd/es/upload';
+import request from '@/common/fileRequest';
+import { ButtonProps } from 'antd/lib/button';
 import './index.less';
 
-function BaseUpload({ text, buttonProps, request, onSuccess, onError }: UploadButtonProps) {
-  const props: PropTypes = {
-    request,
-    onError(errorMsg) {
-      const msg = errorMsg || '导入数据失败！';
-      message.warn(msg);
-      onError && onError(new Error(msg));
-    },
-    onSuccess() {
-      // 导入输入成功有两个提示文案，故将此注释掉--谢小平 不确定是否有影响其他业务
-      // message.success('导入数据成功！');
-      onSuccess && onSuccess(null);
-    }
+const BaseUpload = ({
+  onFinish,
+  listType = 'text',
+  multiple,
+  accept,
+  value,
+  buttonProps,
+  requestUrl,
+  ...props
+}: UploadProps & {
+  buttonProps?: ButtonProps;
+  value?: any;
+  requestUrl: string;
+  onFinish: (data: any) => void;
+}) => {
+  const headers = {
+    Authorization: (window as any).token,
   };
 
+  const typesMap: any = {
+    image: 'image/*',
+    doc: '.doc,.docx,.txt,.pdf,.xlsx,.xls',
+    video: 'video/*',
+    zip: '.rar,.zip',
+    audio: 'audio/*',
+  };
+
+  const [_accept, setAccept] = useState<string>('');
+  const [uploadFiles, setUploadFiles] = useState<RcFile[]>([]);
+
+  useEffect(() => {
+    if (accept) {
+      const acceptArr = accept.split(',');
+      const acceptTypes = acceptArr
+        .map((val) => {
+          return val in typesMap ? typesMap[val] : val;
+        })
+        .filter((val) => {
+          return !!val;
+        })
+        .join(',');
+      setAccept(acceptTypes);
+    }
+  }, [accept]);
+
+  const customRequest = () => {
+    const formData = new FormData();
+    uploadFiles.forEach((file) => {
+      formData.append(`file`, file);
+    });
+    request(requestUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+      data: formData,
+    }).then((res) => {
+      onFinish(res);
+    });
+  };
+
+  const beforeUpload = (file: RcFile, FileList: RcFile[]) => {
+    setUploadFiles(FileList);
+    return false;
+  };
+  const uploadProps = {
+    headers,
+    listType,
+    multiple,
+    accept: _accept,
+    beforeUpload,
+    customRequest,
+    ...props,
+  };
+  useEffect(() => {
+    if (uploadFiles.length > 0) {
+      customRequest();
+    }
+  }, [uploadFiles]);
+
   return (
-    <Upload className="upload-container" {...props}>
-      <Button {...buttonProps}>{text}</Button>
-    </Upload>
+    <div className="upload-container">
+      <Upload {...uploadProps}>
+        <Button {...buttonProps}>上传</Button>
+      </Upload>
+    </div>
   );
-}
+};
 
 export default BaseUpload;
